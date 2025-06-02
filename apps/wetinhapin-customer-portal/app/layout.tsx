84eba700e3905f2f8fe68aa25f9d33dded0a2e73
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-02 19:17:09 +0200

    add Theme provider

--- apps/wetinhapin-customer-portal/app/layout.tsx
+++ apps/wetinhapin-customer-portal/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@workspace/ui/globals.css";
+import {Theme} from "@/components/Theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Theme>
         {children}
+      </Theme>
       </body>
     </html>
   );
