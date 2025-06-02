b59dfe0f76be6fd3a7f6e1c5a3df76ac37539ef9
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-02 18:19:24 +0200

    add ThemeToggle to homepage for testing

--- apps/wetinhapin-helpdesk/app/page.tsx
+++ apps/wetinhapin-helpdesk/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { Button } from "@workspace/ui/components/button"
+import ThemeToggle from "@workspace/ui/components/common/themeToggle";
 
 export default function Page() {
   return (
@@ -7,6 +8,7 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+          <ThemeToggle />
         <Link href="/about">About</Link>
       </div>
     </div>
