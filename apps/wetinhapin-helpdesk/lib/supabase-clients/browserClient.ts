14f2936b7dfb22c591b66e2cd88c5587b62e68f5
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-08 22:15:37 +0200

    create browserClient.ts

--- apps/wetinhapin-helpdesk/lib/supabase-clients/browserClient.ts
+++ apps/wetinhapin-helpdesk/lib/supabase-clients/browserClient.ts
@@ -1,12 +1,10 @@
 import {createBrowserClient} from '@supabase/ssr';
 
-export const supabaseBrowserClient = () => {
-    const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+import {assertEnvVar} from "@/utils/helpers";
 
-    if (!url || !key) {
-        throw new Error("Supabase URL or Key is not defined in environment variables.");
-    }
+export const supabaseBrowserClient = () => {
+    const url = assertEnvVar(process.env.NEXT_PUBLIC_SUPABASE_URL!);
+    const key = assertEnvVar(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
     return createBrowserClient(url, key);
 }
