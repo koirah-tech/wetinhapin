fc6111193982ae9df4eccf920313011be64673c0
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-08 22:09:06 +0200

    remove test useEffect function

--- apps/wetinhapin-helpdesk/app/page.tsx
+++ apps/wetinhapin-helpdesk/app/page.tsx
@@ -1,8 +1,11 @@
+
 import Link from "next/link"
+
 import { Button } from "@workspace/ui/components/button"
 import ThemeToggle from "@workspace/ui/components/common/themeToggle";
 
 export default function Page() {
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
