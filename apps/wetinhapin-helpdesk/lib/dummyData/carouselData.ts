07c385ff04b031a1d733939dc988cb1e5696de2b
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-16 08:25:37 +0200

    add data for carousel

---
+++ apps/wetinhapin-helpdesk/lib/dummyData/carouselData.ts
@@ -0,0 +1,58 @@
+import { CarouselItem } from "@/components/Carousel";
+
+export const DEFAULT_ITEMS: CarouselItem[] = [
+  {
+    title: "MoCTI HQ",
+    description: "The Ministry in action — showcasing Sierra Leone’s drive for tech innovation and communication reform.",
+    id: 1,
+    image: "../../../../images/headoffice-moctisl.webp",
+  },
+  {
+    title: "Learn Coding",
+    description: "Empowering citizens through tech and communication: a glimpse into digital public service.",
+    id: 2,
+    image: "../../../../images/learn-coding-moctisl.webp",
+  },
+  {
+    title: "Energy",
+    description: "Infrastructure and data at the heart of Sierra Leone’s digital vision.",
+    id: 3,
+    image: "../../../../images/energy-moctisl.webp",
+  },
+  {
+    title: "Connecting",
+    description: "Connected and empowered — Orange SL’s role in Sierra Leone’s digital lifestyle",
+    id: 4,
+    image: "../../../../images/connecting-orangesl.webp",
+  },
+  {
+    title: "Communities",
+    description: "Reaching the unreachable — connecting communities and classroom",
+    id: 5,
+    image: "../../../../images/communit-orangesl.webp",
+  },
+  {
+    title: "Telecommunications",
+    description: "Where service meets experience — Orange SL’s customer care in action",
+    id: 6,
+    image: "../../../../images/office-space-orangesl.webp",
+  },
+  {
+    title: "Internet Service",
+    description: "Fast, affordable, and family-focused — Africell in everyday moments",
+    id: 7,
+    image: "../../../../images/internet-service-africellsl.webp",
+  },
+  {
+    title: "Social Media",
+    description: "Powered by connection — Africell and the rhythm of the city.",
+    id: 8,
+    image: "../../../../images/social-media-africellsl.webp",
+  },
+  {
+    title: "Digital Hub",
+    description: "Africell’s digital hub for every Sierra Leonean.",
+    id: 9,
+    image: "../../../../images/digital-hub-africellsl.webp",
+  }
+];
