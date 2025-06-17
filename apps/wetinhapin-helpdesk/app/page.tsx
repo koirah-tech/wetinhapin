98668428402d05b261948ca213013b77e98c8dc0
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-17 16:05:23 +0200

    add changes - import useTheme add the welcome message

--- apps/wetinhapin-helpdesk/app/page.tsx
+++ apps/wetinhapin-helpdesk/app/page.tsx
@@ -1,21 +1,44 @@
+'use client'
 
 import Link from "next/link"
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
 import { Button } from "@workspace/ui/components/button"
 import ThemeToggle from "@workspace/ui/components/common/themeToggle";
-import Carousel from "@/components/Carousel";
+import Carousel from "@/components/login/Carousel";
+import LoginForm from "@/components/login/LoginForm";
 
 export default function Page() {
 
+  const { theme } = useTheme();
+
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-          <ThemeToggle />
-        <Link href="/about">About</Link>
-        <Carousel baseWidth={500} autoplay={true} loop/>
+    <div className="grid gap-8 justify-center px-8">
+      <div className="flex flex-col items-center justify-center gap-30">
+        <div>
+          {/* Logo */}
+          <Image 
+            src={theme === "dark"  ?'/logos/logo-black-background.png' : '/logos/Logo-white-bg.png'}
+            alt="WetinHapin Logo"
+            width={195}
+            height={41}
+          />
+        </div>
+
+        <div className="grid gap-2">
+          <h1 className='h1-bold'>
+              Welcome Back {" "}  
+              <span className='inline-block'> 👋</span>
+          </h1>
+          <p className="w-sm body-regular">
+            Sign in to manage support tickets, collaborate with your team, and help customers faster. 
+          </p>
+        </div>
       </div>
+
+      <LoginForm />
     </div>
   )
 }
